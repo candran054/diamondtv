c75f0a9b01b167feb78e81403eb062179b03e97d
candran054 <candran054@gmail.com>
2023-11-22 14:16:56 +0700

    add sign in component

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -3,12 +3,14 @@ import React from "react";
 export default function SignIn() {
   return (
     <div className="flex h-screen justify-center items-center">
-      <div className="flex flex-col justify-center py-3 text-center w-[360px] rounded-md bg-zinc-400">
+      <div className="flex flex-col justify-center py-3 text-center w-[360px] rounded-md bg-[#fed7aa]/20">
         <p className="text-lg font-semibold">Welcome</p>
-        <p className="mt-6">
+        <p className="mt-6 text-sm max-w-xs mx-auto">
           By continuing, you agree to our User Agreement and Privacy
           Policy.
         </p>
+
+        <p></p>
       </div>
     </div>
   );
