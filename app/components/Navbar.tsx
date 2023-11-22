d8b5671ce8fe1089f36e98cea2b301822cda7dda
candran054 <candran054@gmail.com>
2023-11-22 15:12:54 +0700

    fix navbar center position

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -19,11 +19,11 @@ const links = [
 
 export default function Navbar() {
   return (
-    <div className="flex py-6 bg-slate-900">
+    <div className="flex items-center py-6 bg-slate-900">
       <Link href="/">
-        <Icons.logo className="ml-4 cursor-pointer" />
+        <Icons.logo className="translate-x-4 ml-auto cursor-pointer" />
       </Link>
-      <ul className="flex w-full text-amber-100 justify-center items-center gap-4 cursor-pointer">
+      <ul className="flex mx-auto text-amber-100 gap-4 cursor-pointer">
         {links.map((link) => (
           <li className="link link-underline" key={link.title}>
             <a className="hover:text-white" href={link.link}>
